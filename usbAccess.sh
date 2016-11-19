5d112e8f7261af5da511af650f81a145b83686f4
Maksym Zaitsev <cryptolok@protonmail.com>
2016-11-19 15:53:22 +0100

    Create usbAccess.sh

---
+++ usbAccess.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# usb ports switch to protect from badUSB and RubberDucky
+
+BUS=/sys/bus/usb/drivers_autoprobe
+
+echo '!!! USB OPENED FOR 10 SECONDS !!!'
+echo 1 > $BUS
+
+sleep 10
+
+echo 0 > $BUS
+
+echo '!!! USB BLOCKED !!!'
